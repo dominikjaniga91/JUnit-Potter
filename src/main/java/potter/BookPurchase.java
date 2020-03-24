646dc1e133ec00c5086f770ec58f591e505b0f4c
Dominik <dominikjaniga91@gmail.com>
2020-03-24 18:40:56 +0100

    second test that check shopping amount with discount if customer purchased two book

--- src/main/java/potter/BookPurchase.java
+++ src/main/java/potter/BookPurchase.java
@@ -1,18 +1,38 @@
 package potter;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+
+import java.util.*;
 import java.util.stream.Stream;
 
 public class BookPurchase {
 
-    Map<String, Double> books = Map.of("Kamień Filozoficzny", 8.0);
+    Map<String, Double> bookStore = Map.of("Kamień Filozoficzny", 8.0,
+                                        "Komnata Tajemnic",8.0);
 
+    Map<Integer, Double> discounts = Map.of(1, 1.0,
+                                            2, 0.95,
+                                            3,0.9,
+                                            4,0.8,
+                                            5,0.75);
 
+    public double getShoppingAmount(List<String> bookTitles) {
 
-    public double getShoppingAmount(String bookTitle) {
+        double shoppingAmount = 0.0;
+        int availableBooks = 0;
 
-        return Objects.requireNonNullElse(books.get(bookTitle), 0.0);
+        for (String bookTitle :  bookTitles ) {
+            for (Map.Entry<String,Double> book : bookStore.entrySet()  ) {
+                if(bookTitle.equals(book.getKey())){
+                    shoppingAmount += book.getValue();
+                    availableBooks++;
+                }
+            }
+        }
+
+        shoppingAmount *= discounts.get(availableBooks);
+
+        return shoppingAmount;
     }
+
+
 }
--- src/test/java/potter/BookPurchaseTest.java
+++ src/test/java/potter/BookPurchaseTest.java
@@ -13,11 +13,18 @@ public class BookPurchaseTest {
     BookPurchase book = new BookPurchase();
 
     @Test
+    @DisplayName("Should return book price after purchase only one book")
     void shouldReturnSBookPrice_afterPurchaseOneBook(){
 
-        Assertions.assertThat(book.getShoppingAmount("Kamień Filozoficzny")).isEqualTo(8.0);
+        Assertions.assertThat(book.getShoppingAmount(List.of("Kamień Filozoficzny"))).isEqualTo(8.0);
     }
 
+    @Test
+    @DisplayName("Should return amount with discount 5% after purchase of 2 different books")
+    void shouldReturnAmountWith5Discount_afterPurchase2Books(){
+        List<String> books = List.of("Kamień Filozoficzny","Komnata Tajemnic");
+        Assertions.assertThat(book.getShoppingAmount(books)).isEqualTo(15.2);
+    }
 
 
 
