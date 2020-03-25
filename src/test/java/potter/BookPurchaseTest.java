359b9cc7dae7e410957cc9ae1db09581792bb6f4
Dominik <dominikjaniga91@gmail.com>
2020-03-25 13:42:39 +0100

    changing @DisplayName value in the several tests

--- src/test/java/potter/BookPurchaseTest.java
+++ src/test/java/potter/BookPurchaseTest.java
@@ -44,24 +44,24 @@ public class BookPurchaseTest {
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(30.0);
     }
 
+
     @Test
-    @DisplayName("Should return amount with best discount after purchase of 2 different books and 1 doubled")
-    void shouldReturnAmountWithDiscount_afterPurchase3BooksAnd1TheSame(){
+    @DisplayName("Should return amount with best discount after purchase of 3 books - 2x1 and 1")
+    void shouldReturnAmountWithDiscount_afterPurchase2And1Books(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 , "Więzień Askabanu",1);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(23.2);
     }
 
     @Test
-    @DisplayName("Should return amount with best discount after purchase of 2 units of 2 different books")
+    @DisplayName("Should return amount with best discount after purchase of 4 books - 2x2")
     void shouldReturnAmountWithDiscount_afterPurchase4Books(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 , "Więzień Askabanu",2);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(30.4);
     }
 
-
     @Test
-    @DisplayName("Should return amount with best discount after purchase of 3 different books and 1 doubled")
-    void shouldReturnAmountWithDiscount_afterPurchase2BooksAnd2TheSame(){
+    @DisplayName("Should return amount with best discount after purchase of 4 books - 2x1 and 2x1")
+    void shouldReturnAmountWithDiscount_afterPurchase3And1Books(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 , "Więzień Askabanu",1 , "Czara Ognia",1);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(29.6);
     }
@@ -81,21 +81,21 @@ public class BookPurchaseTest {
     }
 
     @Test
-    @DisplayName("Should return amount with best discount after purchase of 2 different books and 3 doubled")
+    @DisplayName("Should return amount with best discount after purchase of 8 books - 6x2 and 2x1")
     void shouldReturnAmountWithDiscount_afterPurchase5BooksAnd3TheSame(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 ,"Komnata Tajemnic",2 , "Więzień Askabanu",2 , "Czara Ognia",1 , "Zakon Feniksa",1);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(51.2);
     }
 
     @Test
-    @DisplayName("Should return amount with best discount after purchase 23 books - 3x5 books and 2x4 books")
+    @DisplayName("Should return amount with best discount after purchase 23 books - 3x5 and 2x4")
     void shouldReturnAmountWithDiscount_afterPurchase3x5BooksAnd2x4Boks(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",5 ,"Komnata Tajemnic",5 , "Więzień Askabanu",5 , "Czara Ognia",4 , "Zakon Feniksa",4);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(141.2);
     }
 
     @Test
-    @DisplayName("Should return amount with best discount after purchase 6 books - 2 units of each of 3 different books")
+    @DisplayName("Should return amount with best discount after purchase 6 books - 3x2")
     void shouldReturnAmountWithDiscount_afterPurchase2x3Books(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 , "Więzień Askabanu",2 , "Czara Ognia",2);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(43.2);
@@ -109,7 +109,7 @@ public class BookPurchaseTest {
     }
 
     @Test
-    @DisplayName("Should return amount with best discount after purchase 7 books - 5 different and 2 doubled")
+    @DisplayName("Should return amount with best discount after purchase 7 books - 5x1 and 2x1")
     void shouldReturnAmountWithDiscount_afterPurchase7Books(){
         Map<String,Integer> books = Map.of("Kamień Filozoficzny",2 ,"Komnata Tajemnic",2 , "Więzień Askabanu",1 , "Czara Ognia",1 , "Zakon Feniksa",1);
         Assertions.assertThat(book.getShoppingCost(books)).isEqualTo(45.2);
