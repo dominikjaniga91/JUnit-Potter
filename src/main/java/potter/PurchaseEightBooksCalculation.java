b16498c58076028e608b2d03fa84d0f253ff1f1c
Dominik <dominikjaniga91@gmail.com>
2020-03-27 11:47:11 +0100

    create PurchaseEightBooksCalculation and add discountCalculation method

---
+++ src/main/java/potter/PurchaseEightBooksCalculation.java
@@ -0,0 +1,12 @@
+package potter;
+
+import java.util.Map;
+import static potter.BookPurchase.*;
+
+public class PurchaseEightBooksCalculation implements DiscountCalculationStrategy {
+
+    @Override
+    public Double discountCalculation(Map<String, Integer> purchasedBooks) {
+        return 2*4*BOOK_PRICE* discounts.get(4);
+    }
+}
