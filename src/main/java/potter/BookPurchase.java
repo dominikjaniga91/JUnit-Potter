0636b77ed8a67a52df80dda6436745bc76b29799
Dominik <dominikjaniga91@gmail.com>
2020-03-27 12:06:06 +0100

    changes in BookPurchase class: - add DiscountCalculationStrategy variable - creating new object in the if statements and invoke calculation method

--- src/main/java/potter/BookPurchase.java
+++ src/main/java/potter/BookPurchase.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class BookPurchase {
 
     final static double BOOK_PRICE = 8.0;
+    DiscountCalculationStrategy calculation;
 
     static Map<Integer, Double> discounts = Map.of(1, 1.0,
                                             2, 0.95,
@@ -23,48 +24,16 @@ public class BookPurchase {
     public double getShoppingCost(Map<String,Integer> purchasedBooks) {
 
         if(getAmountOfBooks(purchasedBooks) < 8){
-            return defaultDiscountCalculation(purchasedBooks);
+            return new DefaultDiscountCalculation().discountCalculation(purchasedBooks);
         }else if(getAmountOfBooks(purchasedBooks) == 8){
-            return  2*4*BOOK_PRICE* discounts.get(4);
+            return new PurchaseEightBooksCalculation().discountCalculation(purchasedBooks);
         }else{
-            return Math.min(defaultDiscountCalculation(purchasedBooks), alternativeDiscountCalculation(purchasedBooks));
+            return Math.min(
+                    new DefaultDiscountCalculation().discountCalculation(purchasedBooks),
+                    new AlternativeDiscountCalculation().discountCalculation(purchasedBooks)
+            );
         }
 
     }
-
-    public double defaultDiscountCalculation(Map<String,Integer> purchasedBooks) {
-
-        double overallCost = 0.0;
-        int maxUnitsOfSingleBook = gatMaxUnitsOfSingleBook(purchasedBooks);
-
-        for(int i=1; i <= maxUnitsOfSingleBook; i++){
-            int availableBooks = 0;
-            double shoppingAmount = 0.0;
-            for (Map.Entry<String,Integer> book :  purchasedBooks.entrySet()) {
-
-                if(book.getValue() >= i){
-                    shoppingAmount += BOOK_PRICE;
-                    availableBooks++;
-                }
-            }
-            shoppingAmount *= discounts.get(availableBooks);
-            overallCost += shoppingAmount;
-        }
-        return overallCost;
-    }
-
-
-
-    private double alternativeDiscountCalculation(Map<String,Integer> purchasedBooks){
-
-        int amountOfBooks = getAmountOfBooks(purchasedBooks);
-
-        //amountOfBooks = 5*rate + 4*rate + modulo
-        int rate = amountOfBooks / 9;
-        int modulo = amountOfBooks % 9;
-
-        return BOOK_PRICE*(rate*(5* discounts.get(5) + 4*discounts.get(4)) + modulo*discounts.get(modulo));
-    }
-
 
 }
